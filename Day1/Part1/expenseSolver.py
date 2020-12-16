f312cd0abc22969f4f354a3e9d9e47d1a233e932
christophermalinosky <christopher.malinosky@gmail.com>
2020-12-16 16:05:21 -0500

    Fixed bug for numbers in the exact middle of the goal

--- Day1/Part1/expenseSolver.py
+++ Day1/Part1/expenseSolver.py
@@ -5,12 +5,19 @@ def solveExpense(fileName, goal):
     inputFile = os.path.join(folderPath, "input.txt")
     input = open(inputFile, "r")
 
+    halfs = 0
     inputSet = set()
     for inputNumber in input:
-        inputSet.add(int(inputNumber[:-1]))
+        number = int(inputNumber[:-1])
+        inputSet.add(number)
+        if goal % 2 == 0 and goal / 2 == number:
+            halfs += 1
 
     for inputNumber in inputSet:
-        if goal - inputNumber in inputSet:
+        if goal % 2 == 0 and goal / 2 == inputNumber:
+            if halfs >= 2:
+                return inputNumber * (goal - inputNumber)
+        elif goal - inputNumber in inputSet:
             return inputNumber * (goal - inputNumber)
 
 print(solveExpense("input.txt", 2020))
